1b2c967ffb257d788141aa6976476b505ebd3152
Olga_Askasim <uthfcbvjdf2015@yandex.ru>
2024-06-13 21:22:11 +0300

    change test.py

--- track_number_create_test.py
+++ track_number_create_test.py
@@ -7,7 +7,10 @@ def get_track_number_of_order():
     return track_number.json()["track"]
 
 
-def test_create_and_track_order():
+def positive_assert():
     track_number = get_track_number_of_order()
     get_response = sending_requests.get_order_info(track_number)
     assert get_response.status_code == 200
+
+def test_order():
+    positive_assert()
